d52b4563f3c2397ebcc98d2ea040e66f30760619
Tobias Lehtipalo <tobias.lehtipalo@gmail.com>
2022-02-01 00:43:11 +0100

    first working timeline

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { DataView } from "spotfire-api";
+import { DataView, DataViewRow } from "spotfire-api";
+import * as d3 from "d3";
 
 const Spotfire = window.Spotfire;
 const DEBUG = false; 
+
+const timeAxisName = "Time";
+const titleAxisName = "Title";
+const descriptionAxisName = "Description";
+const minimumTimeMarkerWidth = 50;
+const timeMarkermargin = 50;
+const timelineHeight = 25;
+const cardWidth = 150;
+const cardHeight = 100; 
 
 Spotfire.initialize(async (mod) => {
     /**
@@ -16,9 +26,107 @@ Spotfire.initialize(async (mod) => {
         mod.controls.errorOverlay.show(err);
     });
 
+    const modContainer = d3.select("#mod-container");
+
     async function onChange(dataView: DataView, windowSize: Spotfire.Size) {
 
+        const hasTime = !!(await dataView.categoricalAxis(timeAxisName));
+
+        if (!hasTime) return;
+
+        const timeLeaves = (await (await dataView.hierarchy(timeAxisName))?.root())?.leaves() || [];
+        const rows = await dataView.allRows() || [];
+
+        let timeLineTop = windowSize.height/2 - timelineHeight/2;
+        let timeMarketWidth = (windowSize.width-timeMarkermargin*2)/timeLeaves.length;
+        let timeMarkerWidth = timeMarketWidth < minimumTimeMarkerWidth ? 50 : timeMarketWidth;
+
         // Update mod display
+
+        let timeline = 
+        modContainer.selectAll(".timeline")
+        .data([null])
+        .join("div")
+        .attr("class","timeline")
+        .attr("style", (d, i) => `top:${timeLineTop}px; width:${timeMarkermargin*2+timeLeaves.length*timeMarkerWidth}px`);
+
+
+        timeline.selectAll(".timeMarker")
+        .data(timeLeaves)
+        .join("div")
+        .attr("class","timeMarker")
+        .attr("style", (d:Spotfire.DataViewHierarchyNode, i) => `left:${timeMarkermargin+i * timeMarkerWidth}px; width:${timeMarkerWidth}px`)
+        .text(d => d.formattedValue());
+
+        modContainer.selectAll(".connector")
+        .data(rows)
+        .join("div")
+        .attr("class","connector")
+        .attr("style", (d:DataViewRow,i) => {
+
+            let left = timeMarkermargin+d.categorical(timeAxisName).leafIndex*timeMarkerWidth+timeMarkerWidth/2;
+            let top = timeLineTop;
+            let height = 0;
+
+            switch  (d.categorical(timeAxisName).leafIndex % 4) {
+                case 0: 
+                    top = top - timelineHeight; 
+                    height = timelineHeight;
+                break;
+                case 1: 
+                    top = top + timelineHeight;
+                    height = timelineHeight; 
+                    break;
+                case 2: 
+                    top = top - cardHeight-timelineHeight*2;
+                    height = cardHeight+timelineHeight*2;
+                    break;
+                case 3: 
+                    top = top + timelineHeight; 
+                    height = timelineHeight*3+cardHeight;
+                    break;
+            }
+            
+            return `
+            left:${left}px; 
+            top:${top}px;
+            height:${height}px;
+            width:${2}px
+            `
+
+        })
+        
+
+        modContainer.selectAll(".card")
+        .data(rows)
+        .join("div")
+        .attr("class","card")
+        .attr("style", (d:DataViewRow, i) => {
+
+            let left = timeMarkermargin+d.categorical(timeAxisName).leafIndex*timeMarkerWidth-cardWidth/2+timeMarkerWidth/2;
+            let top = timeLineTop;
+
+            switch  (d.categorical(timeAxisName).leafIndex % 4) {
+                case 0: top = top - cardHeight-timelineHeight; break;
+                case 1: top = top + timelineHeight*2; break;
+                case 2: top = top - cardHeight*2-timelineHeight*2;break;
+                case 3: top = top + cardHeight+timelineHeight*3; break;
+            }
+            
+            return `
+            left:${left}px; 
+            top:${top}px
+            `
+        })
+        .html(d => {
+            
+            let s = `<strong>${d.categorical(titleAxisName).formattedValue()}</strong>
+            <br>
+            ${d.categorical(descriptionAxisName).formattedValue()}
+            `
+                        return s;
+        })
+        
 
         context.signalRenderComplete();
     }
